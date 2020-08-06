8e901db5df8ee8154f270923e3e5f65216ef7906
salaarkhan-dev <salaarkhan311@gmail.com>
2020-08-06 04:30:36 -0700

    Created Project Model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField(max_length=200, blank=True)
+    date = models.DateField()
