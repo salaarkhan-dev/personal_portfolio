fb94ec660d238161137e75eae2090500b4410cfb
salaarkhan-dev <salaarkhan311@gmail.com>
2020-08-06 08:55:52 -0700

    order blogs by date

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
