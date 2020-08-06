faf1aed31e4febf41270a99368c625a8a3bf8882
salaarkhan-dev <salaarkhan311@gmail.com>
2020-08-06 08:43:25 -0700

    added details

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    description = models.TextField(max_length=5000)
+    url = models.URLField(max_length=200)
