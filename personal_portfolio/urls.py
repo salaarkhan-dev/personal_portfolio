fa1043bae1ba6af113c941239dc18703ca91376e
salaarkhan-dev <salaarkhan311@gmail.com>
2020-08-06 22:27:09 -0700

    Fixed Projects

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls'), name='blog'),
+    path('404/', views.notfound, name='404'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    url = models.URLField(max_length=200, blank=True)
+    url = models.URLField(max_length=200)
     image = models.ImageField(upload_to='portfolio/images/')
     date = models.DateField()
     tags = models.CharField(max_length=20)
