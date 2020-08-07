3c836a5b8eacace1ea3a40d858cd1ee896922bfc
salaarkhan-dev <salaarkhan311@gmail.com>
2020-08-07 09:07:26 -0700

    Added Rich Editor

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField()
-    description = models.TextField(max_length=5000)
+    description = RichTextField(blank=True, null=True)
     image = models.ImageField(upload_to='blog/images/')
     tags = models.CharField(max_length=20)
 
