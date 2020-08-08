bde246806d275a2f5984115991c16a64ef13560d
salaarkhan-dev <salaarkhan311@gmail.com>
2020-08-07 21:47:18 -0700

    added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def all_blogs(request):
     blogs = Blog.objects.order_by('-date')
+
+    paginator = Paginator(blogs, 3)
+    page = request.GET.get('page')
+    blogs = paginator.get_page(page)
+
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
